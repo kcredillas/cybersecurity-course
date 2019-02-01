dffa19e09077fbb7a459e605858015a86a08cb0d
Kenneth Redillas <kcredillas@mix.wvu.edu>
2019-02-01 15:58:43 -0500

    added des.py

--- pa1/des.py
+++ pa1/des.py
@@ -1,10 +1,11 @@
 import re
+
 def textprocessing():
-    text = "Today is Tuesday"
-    match = re.split("[^a-zA-z0-9]", text)
-    processed = ""
-    for word in match:
-        processed += word
-    print (processed)
+	text = "Today is Tuesday"
+	match = re.split("[^a-zA-Z0-9]", text)
+	processed = ""
+	for word in match:
+		processed += word
+	print(processed)
 
 textprocessing()
