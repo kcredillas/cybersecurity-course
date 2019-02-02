05e7071e9df006b5d70eff96b8b1d527bf7dbd26
Kenneth Redillas <kcredillas@mix.wvu.edu>
2019-02-02 12:16:45 -0500

    finished text processing

--- pa1/des.py
+++ pa1/des.py
@@ -1,11 +1,34 @@
 import re
 
-def textprocessing():
-	text = "Today is Tuesday"
+def textprocessing(text):
+	#text = "Today is Tuesday"
 	match = re.split("[^a-zA-Z0-9]", text)
 	processed = ""
 	for word in match:
 		processed += word
-	print(processed)
+	#print(processed)
+	return processed
 
-textprocessing()
+def ascii2bin(s=''):
+	b = []
+	n = len(s)
+	if n > 16:
+		print("Error: The size of your plain text is more than 64 bits!!!!")
+
+	if n < 16:
+		diff = 16 - n
+		pad = "\0" * diff
+		s += pad
+		
+	b =[bin(ord(x))[2:].zfill(8) for x in s]
+	return b
+
+def main():	
+	text = input("What text would you like to encrypt?\r\n")
+	text = textprocessing(text)
+	binText = ascii2bin(text)
+	for x in binText:
+		print(x)
+
+if __name__ == '__main__':
+	main()
