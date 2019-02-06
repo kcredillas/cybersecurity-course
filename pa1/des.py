cd3bc198a1669761c22853fda9eb461b1dc61e9f
Kenneth Redillas <kcredillas@mix.wvu.edu>
2019-02-06 14:18:14 -0500

    finished XOR and started S_BOX computation

--- pa1/des.py
+++ pa1/des.py
@@ -17,6 +17,76 @@ SHIFTS = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
 
 #E_BIT_SELECTION TABLE
 E_BIT_SELECTION = [32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1] 
+
+#S BOX TABLES
+S_BOX = [
+# Box 1
+[
+[14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
+[0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8],
+[4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0],
+[15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]
+],
+# Box 2
+
+[
+[15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10],
+[3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5],
+[0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15],
+[13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9]
+],
+
+# Box 3
+
+[
+[10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8],
+[13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1],
+[13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7],
+[1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]
+
+],
+
+# Box 4
+[
+[7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15],
+[13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9],
+[10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4],
+[3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14]
+],
+
+# Box 5
+[
+[2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9],
+[14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6],
+[4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14],
+[11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3]
+],
+# Box 6
+
+[
+[12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11],
+[10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8],
+[9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6],
+[4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13]
+
+],
+# Box 7
+[
+[4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1],
+[13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6],
+[1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2],
+[6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12]
+],
+# Box 8
+
+[
+[13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7],
+[1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2],
+[7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8],
+[2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]
+]
+
+]
 def textprocessing(text):
 	#text = "Today is Tuesday"
 	match = re.split("[^a-zA-Z0-9]", text)
@@ -25,7 +95,6 @@ def textprocessing(text):
 		processed += word
 	#print(processed)
 	return processed
-
 def ascii2bin(s=''):
 	b = []
 	n = len(s)
@@ -39,7 +108,6 @@ def ascii2bin(s=''):
 		
 	b =[bin(ord(x))[2:].zfill(8) for x in s]
 	return b
-
 def getParity(n):
 	counter = 0
 	for ones in n:
@@ -48,17 +116,14 @@ def getParity(n):
 	if counter % 2 == 0:
 		return 1 #EVEN PARITY
 	return 0 #ODD PARITY 
-
 def permuteKey(masterKey, table):
 	compressedKey = ''
 	for i in table:
 		compressedKey += masterKey[i-1]
 	return compressedKey
-
 def leftShift(bits, numberOfLeftShifts):
 	newBits = bits[numberOfLeftShifts:] + bits[:numberOfLeftShifts]
 	return newBits
-
 def permute(block, table):
 	permutedBlock = ''
 	for i in table:
@@ -81,9 +146,19 @@ def keyGeneration(key):
 	return subKeys
 def functionF(bit32text, bit48key):
 	E = permute(bit32text, E_BIT_SELECTION)
+	print("Expansion permutation:\n" + E)
 	R_iminus1 = E
-	s_input = XOR(R_iminus1, bit48key)
-	#SUB BOX
+	xor_result = XOR(R_iminus1, bit48key)
+	#S BOX Computation
+	#S_BOX[boxnumber][rownumber][column_number]
+	#[0:8], [8:16], [16:24], ...
+	s_input = list()
+	for i in range(0, len(xor_result), 6):
+		s_input.append(xor_result[i:i+6])
+	print("XOR with key:")
+	for i in range(len(s_input)):
+		print(s_input[i])
+	return
 	#P function
 	#return output
 def XOR(A,B):
@@ -94,7 +169,6 @@ def XOR(A,B):
 		else:
 			xor += '1'
 	return xor
-	
 def main():	
 	#Text preprocessing
 	text = input("What text would you like to encrypt?\r\n")
@@ -149,6 +223,7 @@ def main():
 	#R0 goes to L1
 	#X goes to R1 
 	#etc
+	F = functionF(R0, roundKeys[0])
 
 		
 if __name__ == '__main__':
